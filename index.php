7eb7113d0bf1a5181c08965a3ac7eb40c160456f
The Hunger Project <john.coonrod@thp.org>
2020-05-08 15:13:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // Default router under php 7.2 on GAE
+// This version doesn't handle logins
 $path=$_SERVER['REQUEST_URI'];
 $q=strpos($path,"?"); // strip off GET string if needed
 if($q) $path=substr($path,0,$q);
