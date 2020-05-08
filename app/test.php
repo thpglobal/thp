734d57d30aa3c51d63a1e7d692dd85a2d4a1eda8
The Hunger Project <john.coonrod@thp.org>
2020-05-08 14:47:47 -0400

    Update test.php

--- app/test.php
+++ app/test.php
@@ -1,13 +1,18 @@
 <?php
 // Test the stickyness of filters using cookies instead of session variables
 require(__DIR__."/../classes/thp_classes.php");
+
+$pairs=array("1"=>"Apple","2"=>"Banana","3"=>"Tomato");
+
 $page=new Page;
 $page->start("Test sticky filters!");
 $filter=new Filter;
 $filter->start();
-echo("<p>Toggle: ".$filter->toggle("toggle")."</p>\n");
-echo("<p>Year: ".$filter->range("year",2008,2030)."</p>\n");
-$pairs=array("1"=>"Apple","2"=>"Banana","3"=>"Tomato");
-echo("<p>Fruit: ".$filter->pairs("fruit",$pairs)."</p>\n");
+$fruit=$filter->pairs("fruit",$pairs)
+$year=$filter->range("year",2008,2030);
+$toggle=$filter->toggle("toggle");
+echo("<p>Toggle: $toggle</p>\n");
+echo("<p>Year: $year</p>\n");
+echo("<p>Fruit $fruit: ".$pairs[$fruit]."</p>\n");
 $filter->end();
 $page->end();
