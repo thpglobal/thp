009f8f8887f35f839aa4cf43aac7600673b5b759
The Hunger Project <john.coonrod@thp.org>
2020-05-08 13:20:08 -0400

    Update test.php

--- app/test.php
+++ app/test.php
@@ -1,3 +1,10 @@
 <?php
-// Test the stickyness of forms using cookies instead of session variables
-
+// Test the stickyness of filters using cookies instead of session variables
+require(__DIR__."/../classes/thp_classes.php");
+$page=new Page;
+$page->start("Test sticky filters!");
+$filter=new Filter;
+$filter->start();
+$filter->toggle
+$filter->end();
+$page->end();
