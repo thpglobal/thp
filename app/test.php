ae902683ccf7cbe8b3dd0ec6fc55eaeb56f95ed8
The Hunger Project <john.coonrod@thp.org>
2020-05-08 13:17:45 -0400

    Update test.php

--- app/test.php
+++ app/test.php
@@ -1,4 +1,3 @@
 <?php
-$json=file_get_contents("https://storage.googleapis.com/thp/stuff.json");
-$a=json_decode($json,TRUE);
-print_r($a);
+// Test the stickyness of forms using cookies instead of session variables
+
