50d25fc212e3cfc40bac1c88d96ecc44e79dfd52
John Coonrod <john.coonrod@thp.org>
2021-04-27 12:15:35 -0400

    fix collapse

--- classes/page.php
+++ classes/page.php
@@ -37,7 +37,7 @@ class Page {
 		if(isset($_SESSION["menu"]) and sizeof($menu)>0) { 
 		?>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
